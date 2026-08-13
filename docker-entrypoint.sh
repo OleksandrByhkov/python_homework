6379f784b70eaa9d2be94773f6b0628acb16f672
Oleksandr Byhkov <bychkovshura924@gmail.com>
2026-08-13 23:40:10 +0300

    Add database connection diagnostics

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 echo "Waiting for PostgreSQL..."
+echo "POSTGRES_HOST=$POSTGRES_HOST"
+echo "POSTGRES_PORT=$POSTGRES_PORT"
 
-while ! python -c "import socket; socket.create_connection(('db', 5432), timeout=2)"; do
-  sleep 1
+while ! python -c "import os, socket; socket.create_connection((os.getenv('POSTGRES_HOST'), int(os.getenv('POSTGRES_PORT', '5432'))), timeout=2)"; do
+    sleep 1
 done
 
 echo "PostgreSQL is ready"
